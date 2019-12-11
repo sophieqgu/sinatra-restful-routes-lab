624cd797ec1d870a49d34a5348f53eb07cad0c8d
Sophie Gu <sophieqgu@gmail.com>
2019-12-11 05:35:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
   end
 
   #update 
-  post '/recipes/:id/edit' do 
+  get '/recipes/:id/edit' do 
     @recipe = Recipe.find_by_id(params[:id])
     erb :edit
   end
